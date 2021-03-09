572dd807492ebc9c2b065095d8d8152b8535f628
Johnathan LeBlanc <john.leblanc03@gmail.com>
2021-03-08 21:05:50 -0500

    First commit

---
+++ src/modules/navBar.js
@@ -0,0 +1,18 @@
+const NavBar = () => {
+  return ( 
+    <header>
+      <nav>
+        <div>Logo</div>
+        <div>
+          <ul>
+            <li>Link 1</li>
+            <li>Link 2</li>
+            <li>Link 3</li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+   );
+}
+ 
+export default NavBar;
