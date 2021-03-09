1c6a6dc4f193470540ab7a3570590bd88989b360
Thomas <thomas.hawkins@thinkresearch.com>
2021-03-08 23:24:27 -0500

    add redux to project

---
+++ src/store.js
@@ -0,0 +1,15 @@
+import { compose, createStore, applyMiddleware } from 'redux';
+// import logger from 'redux-logger';
+import createSagaMiddleWare from 'redux-saga';
+import reducers from './reducers';
+
+
+const sagaMiddleWare = createSagaMiddleWare();
+
+const store = createStore(
+  reducers,
+  compose(
+    applyMiddleware(sagaMiddleWare)
+  ));
+
+export default store;
