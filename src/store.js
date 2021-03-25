27882bdf5e85f83d26e8deddedb754f00d5132fe
Thomas Hawkins <Thomas.hawkins09@gmail.com>
2021-03-24 20:26:59 -0400

    added redux thunk cause Im too lazy to learn saga

--- src/store.js
+++ src/store.js
@@ -1,15 +1,13 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 // import logger from 'redux-logger';
-import createSagaMiddleWare from 'redux-saga';
+import thunk from 'redux-thunk';
 import reducers from './reducers';
 
-
-const sagaMiddleWare = createSagaMiddleWare();
 
 const store = createStore(
   reducers,
   compose(
-    applyMiddleware(sagaMiddleWare),
+    applyMiddleware(thunk),
   ),
 );
 
