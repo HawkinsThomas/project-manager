2cc4385be34bba46a226d0ecbc0dfdb72759b4a0
Johnathan LeBlanc <john.leblanc03@gmail.com>
2021-08-03 12:59:13 -0400

    Divided Add Client form into tabs

--- src/forms/AddClient/component.js
+++ src/forms/AddClient/component.js
@@ -8,6 +8,8 @@ import { StyledForm } from 'forms/styledForm';
 
 
 const AddClientForm = ({ userID }) => {
+  const [tab, setTab] = useState(0);
+
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [jobTitle, setJobTitle] = useState('');
@@ -21,6 +23,10 @@ const AddClientForm = ({ userID }) => {
   const [addressCountry, setAddressCountry] = useState('');
 
   const dispatch = useDispatch();
+
+  const toggleTab = (value) => {
+    setTab(value);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -46,97 +52,121 @@ const AddClientForm = ({ userID }) => {
 
   return (
     <StyledForm>
+      <div className="controls">
+        <button
+          type="button"
+          className="button-light tab-button"
+          onClick={() => toggleTab(0)}
+          disabled={tab === 0}
+        >
+          Client Information
+        </button>
+        <button
+          type="button"
+          className="button-light tab-button"
+          onClick={() => toggleTab(1)}
+          disabled={tab === 1}
+        >
+          Company Information
+        </button>
+      </div>
       <form onSubmit={handleSubmit}>
-        <h4>Client Information</h4>
-        <b>First Name</b>
-        <input
-          id="name"
-          type="text"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-          required
-        />
-        <b>Last Name</b>
-        <input
-          id="last-name"
-          type="text"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-          required
-        />
-        <b>Job Title</b>
-        <input
-          id="job-title"
-          type="text"
-          value={jobTitle}
-          onChange={(e) => setJobTitle(e.target.value)}
-          required
-        />
-        <b>Email</b>
-        <input
-          id="email"
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <b>Phone</b>
-        <input
-          id="phone"
-          type="text"
-          value={phone}
-          onChange={(e) => setPhone(e.target.value)}
-          required
-        />
-        <h4>Company Information</h4>
-        <b>Company Name</b>
-        <input
-          id="company-name"
-          type="text"
-          value={company}
-          onChange={(e) => setCompany(e.target.value)}
-          required
-        />
-        <b>Address</b>
-        <input
-          id="street"
-          type="text"
-          value={addressStreet}
-          onChange={(e) => setAddressStreet(e.target.value)}
-          required
-        />
-        <b>City</b>
-        <input
-          id="city"
-          type="text"
-          value={addressCity}
-          onChange={(e) => setAddressCity(e.target.value)}
-          required
-        />
-        <b>Province</b>
-        <input
-          id="province"
-          type="text"
-          value={addressProvince}
-          onChange={(e) => setAddressProvince(e.target.value)}
-          required
-        />
-        <b>Postal Code</b>
-        <input
-          id="postal-code"
-          type="text"
-          value={addressPostalCode}
-          onChange={(e) => setAddressPostalCode(e.target.value)}
-          required
-        />
-        <b>Country</b>
-        <input
-          id="country"
-          type="text"
-          value={addressCountry}
-          onChange={(e) => setAddressCountry(e.target.value)}
-          required
-        />
+        {tab === 0 && (
+          <div>
+            <b>First Name</b>
+            <input
+              id="name"
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+            />
+            <b>Last Name</b>
+            <input
+              id="last-name"
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              required
+            />
+            <b>Job Title</b>
+            <input
+              id="job-title"
+              type="text"
+              value={jobTitle}
+              onChange={(e) => setJobTitle(e.target.value)}
+              required
+            />
+            <b>Email</b>
+            <input
+              id="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <b>Phone</b>
+            <input
+              id="phone"
+              type="text"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              required
+            />
+          </div>
+        )}
+        {tab === 1 && (
+          <div>
+            <b>Company Name</b>
+            <input
+              id="company-name"
+              type="text"
+              value={company}
+              onChange={(e) => setCompany(e.target.value)}
+              required
+            />
+            <b>Address</b>
+            <input
+              id="street"
+              type="text"
+              value={addressStreet}
+              onChange={(e) => setAddressStreet(e.target.value)}
+              required
+            />
+            <b>City</b>
+            <input
+              id="city"
+              type="text"
+              value={addressCity}
+              onChange={(e) => setAddressCity(e.target.value)}
+              required
+            />
+            <b>Province</b>
+            <input
+              id="province"
+              type="text"
+              value={addressProvince}
+              onChange={(e) => setAddressProvince(e.target.value)}
+              required
+            />
+            <b>Postal Code</b>
+            <input
+              id="postal-code"
+              type="text"
+              value={addressPostalCode}
+              onChange={(e) => setAddressPostalCode(e.target.value)}
+              required
+            />
+            <b>Country</b>
+            <input
+              id="country"
+              type="text"
+              value={addressCountry}
+              onChange={(e) => setAddressCountry(e.target.value)}
+              required
+            />
+          </div>
+        )}
         <hr />
         <input
           type="submit"
--- src/forms/styledForm.js
+++ src/forms/styledForm.js
@@ -1,4 +1,22 @@
 import styled from 'styled-components';
 
 
-export const StyledForm = styled.div``;
+export const StyledForm = styled.div`
+  .controls {
+    display: flex;
+    justify-content: space-between;
+    padding-bottom: 1rem;
+    border-bottom: 2px solid ${(props) => props.theme.color.light};
+    margin-bottom: 1rem;
+  }
+
+  .tab-button {
+    width: 49%;
+
+    :disabled {
+      color: white;
+      background-color: ${(props) => props.theme.color.medium};
+      cursor: default;
+    }
+  }
+`;
