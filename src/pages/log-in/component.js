fca5fe71a306f8de22a517acf02acccd93a09498
Johnathan LeBlanc <john.leblanc03@gmail.com>
2021-11-04 23:28:15 -0400

    Pre-merge

--- src/pages/log-in/component.js
+++ src/pages/log-in/component.js
@@ -2,22 +2,22 @@ import React from 'react';
 import Heading from 'modules/Heading';
 import LoginForm from 'modules/forms/LoginForm';
 
+import { StyledComponent } from './styledComponent';
+
 
 const Login = () => (
-  <div>
+  <StyledComponent>
     <Heading title="Log In" />
-    <div className="row">
-      <div className="col-md-6 p-main">
+    <section>
+      <div>
         <h2>Welcome Back!</h2>
-        <p className="focus-text color-medium">
-          Log back in to get back to managing your projects.
-        </p>
+        <p>Log back in to get back to managing your projects.</p>
       </div>
-      <div className="col-md-6 row p-main bg-light">
-        <LoginForm className="col-8 card p-2" />
+      <div>
+        <LoginForm />
       </div>
-    </div>
-  </div>
+    </section>
+  </StyledComponent>
 );
 
 export default Login;
